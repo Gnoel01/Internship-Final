190df4a699392f59bd32e5eec46e041c71ae756f
Gnoel01 <Gerthonoel@gmail.com>
2024-07-22 09:09:39 -0600

    Fixed toggle switch

--- presentation/src/components/DarkMode.js
+++ presentation/src/components/DarkMode.js
@@ -1,25 +1,25 @@
 import React from "react";
-import {ReactComponent as Sun} from "./Sun.svg"
-import {ReactComponent as Moon} from "./Moon.svg"
-import "./Darkmode.css";
+import Sun from "./Sun.png"
+import Moon from "./Moon.png"
+import "./DarkMode.css";
 
 export default function DarkMode () {
-    const setDarkmode = () => {
+    const setDarkMode = () => {
         document.querySelector("body").setAttribute('data-theme', 'dark')
-        localStorage.setitem("selectedTheme", "dark")
+        localStorage.setItem("selectedTheme", "dark")
     }
     const setLightmode = () => {
         document.querySelector("body").setAttribute('data-theme', 'light')
-        localStorage.setitem("selectedTheme", "light")
+        localStorage.setItem("selectedTheme", "light")
     }
 
-    const selected = localSorage.getItem("selectedTheme")
+    const selectedTheme = localStorage.getItem("selectedTheme")
 
     if (selectedTheme === "dark"){
         setDarkMode();
     }
     const toggleTheme = (e) => {
-        if (e.target.checked) setDarkmode();
+        if (e.target.checked) setDarkMode();
         else setLightmode()
     }
     return(
@@ -32,8 +32,10 @@ export default function DarkMode () {
                 defaultChecked={selectedTheme === "dark"}
             />
             <label className='dark_mode_label' for='darkmode-toggle'>
-                <Sun/>
-                <Moon/>
+                {/* <Sun/> */}
+                <img src={Sun} className="App-Sun" alt="Sun"/>
+                {/* <Moon/> */}
+                <img src={Moon} className="App-Moon" alt="Moon"/>
             </label>
         </div>
     );
